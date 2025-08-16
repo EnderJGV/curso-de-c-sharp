5f3584d9b4e5d4c7d4077e3374d8d6c4c5ffa6fb
Ender Guaina <ender.guaina@gmail.com>
2025-08-16 17:18:54 -0300

    Tuplas + Nugets

---
+++ Dio pro/Nuget/Program.cs
@@ -0,0 +1,19 @@
+﻿using Nuget.Models;
+using Newtonsoft.Json;
+
+DateTime dataAtual = DateTime.Now;
+
+List<Vendas> listaVendas = new List<Vendas>();
+
+Vendas v1 = new Vendas(1, "Material de Escritorio", 25.0m, dataAtual);
+Vendas v2 = new Vendas(2, "Lincença de Software", 110.0m, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivo/vendas.json", serializado);
+
+Console.WriteLine(serializado);
+
---
+++ Dio pro/Tuplas_na_pratica/Program.cs
@@ -0,0 +1,35 @@
+﻿// (int, string, string, decimal) tupla = (1, "Pedro", "Machado", 1.80m); // Maneita mais recomendada seria esta
+// // ValueTuple<int, string, string> outroExemploTupla = (1, "Pepe", "Machado");
+// // var outroExemploTupleCreate = Tuple.Create(1, "Leonardo", "Buta", 1.80m);
+
+// Console.WriteLine($"Id: {tupla.Item1}");
+// Console.WriteLine($"Nome: {tupla.Item2}");
+// Console.WriteLine($"sobrenome: {tupla.Item3}");
+// Console.WriteLine($"Altura: {tupla.Item4}");
+
+using Tuplas_na_pratica.Models;
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var (sucesso, linhasArquivo, _quantidadeDeLinas) = arquivo.LerArquivo("Arquivo/arquivo.txt");
+
+if (sucesso)
+{
+    foreach (string linhas in linhasArquivo)
+    {
+        Console.WriteLine(linhas);
+    }
+}
+else
+{
+    Console.WriteLine("Erro ao ler arquivo");
+}
+
+Console.WriteLine("----------------OPERADOR TERNARIO----------------");
+
+int numero = 20;
+bool ehPar = false;
+
+ehPar = numero % 2 == 0;
+
+Console.WriteLine("O numero e par?" + (ehPar ? "SIM" : "NAO"));
